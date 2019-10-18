f1e9719c15e1e5ce72882f28ba3d0cd582cc3436
addNaNs <N3gy3I2107>
2019-10-18 14:56:34 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Sat s = new Sat(15,30,45);
+        s.Ispisi();
+        s.Sljedeci();
+        s.Ispisi();
+        s.PomjeriZa(-48);
+        s.Ispisi();
+        s.Postavi(0,0,0);
+        s.Ispisi();
+    }
+}
---
+++ src/ba/unsa/etf/rpr/Sat.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.rpr;
+
+public class Sat {
+    int sati,minute,sekunde;
+
+    Sat(int sati, int minute, int sekunde) { Postavi(sati,minute,sekunde); }
+    void Postavi(int satiT, int minuteT, int sekundeT) { sati=satiT; minute=minuteT; sekunde=sekundeT; }
+    void Sljedeci() {
+        sekunde++;
+        if (sekunde==60) { sekunde=0; minute++; }
+        if (minute==60) { minute=0; sati++; }
+        if (sati==24) sati=0;
+    }
+    void Prethodni() {
+        sekunde--;
+        if (sekunde==-1) { sekunde=59; minute--; }
+        if (minute==-1) { minute=59; sati--; }
+        if (sati==-1) sati=23;
+    }
+    void PomjeriZa(int pomak) {
+        if (pomak > 0) for (int i = 0; i < pomak; i++) Sljedeci();
+        else for (int i = 0; i < -pomak; i++) Prethodni();
+    }
+    int DajSate()  { return sati; }
+    int DajMinute()  { return minute; }
+    int DajSekunde()  { return sekunde; }
+    void Ispisi()  { System.out.println(sati + ":" + minute + ":" + sekunde); }
+}
